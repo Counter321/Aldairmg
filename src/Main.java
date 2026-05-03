53163534580c353cf6e8f4c85ffe4dac25895685
Counter321 <gonzales2006mg@gmail.com>
2026-05-03 14:22:24 +0200

    Refactorización de variables y métodos.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Cálculo 1:");
-        double b1 = 5.0;
-        double a1 = 10.0;
-        double res1 = b1 * a1;
-        System.out.println("Área: " + res1);
+        calcularYmostrarArea(5.0, 10.0);
 
         System.out.println("Cálculo 2:");
-        double b2 = 8.0;
-        double a2 = 3.0;
-        double res2 = b2 * a2;
-        System.out.println("Área: " + res2);
+        calcularYmostrarArea(8.0, 3.0);
+    }
+
+    private static void calcularYmostrarArea(double baseRectangulo1, double alturaRectangulo1) {
+        // Borra las líneas que repetían "double ..."
+        double res1 = baseRectangulo1 * alturaRectangulo1;
+        System.out.println("Área: " + res1);
     }
 }
