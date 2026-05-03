175bf70bf5faddf0b918f0f0c3f9d6e7b67412ba
Counter321 <gonzales2006mg@gmail.com>
2026-05-03 13:07:10 +0200

    Commit inicial

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Cálculo 1:");
+        double b1 = 5.0;
+        double a1 = 10.0;
+        double res1 = b1 * a1;
+        System.out.println("Área: " + res1);
+
+        System.out.println("Cálculo 2:");
+        double b2 = 8.0;
+        double a2 = 3.0;
+        double res2 = b2 * a2;
+        System.out.println("Área: " + res2);
+    }
+}
